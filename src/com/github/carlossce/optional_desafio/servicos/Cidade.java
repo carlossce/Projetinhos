3b018a53758f93ea0e627c5d471be991581e8118
Carlos Eduardo <151682642+Castroox7@users.noreply.github.com>
2024-12-23 14:36:59 -0300

    Atutu Optional

---
+++ src/com/github/carlossce/optional_desafio/servicos/Cidade.java
@@ -0,0 +1,4 @@
+package com.github.carlossce.optional_desafio.servicos;
+
+public record Cidade(String nome) {
+}
---
+++ src/com/github/carlossce/optional_desafio/servicos/TipoDeResidenciaInvalidaException.java
@@ -0,0 +1,8 @@
+package com.github.carlossce.optional_desafio.servicos;
+
+public class TipoDeResidenciaInvalidaException extends RuntimeException {
+
+    public TipoDeResidenciaInvalidaException() {
+    }
+
+}
